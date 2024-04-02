8d4e03839e9fa05f0f2c8ad5857e2d44edd02b8e
Gabriel <119896525+LucasGabrielfl0@users.noreply.github.com>
2024-04-02 17:17:01 -0300

    Update main.cpp
    Update print and some other Inverter Methods

--- main.cpp
+++ main.cpp
@@ -2,78 +2,77 @@
  * Capibarib E-racing
  * Federal University of Pernambuco (UFPE)
  * Group Area: Powertrain
+ 
+ * This file contains the Integrated VCU (VEHICLE CONTROL UNIT) that unites the
+ * CAN communication with Inverters, the analog communication with all the sensors and 
+ * the Electronic Differential Closed Loop control
  ***/
 
 #include "mbed.h"
-#include "MPU9250.h"
+#include "motor_can.h"
 #include "angle_sensor.h"
-#include "time.h"
-#include "CAN.h"
 #include <cstdint>
+// main() runs in its own thread in the OS
 
 /*===================================== PORTS (STM32 F746ZG)=====================================*/
-#define Volante_in      PC_2
-#define BSE_in          PA_0
-#define APPS1_in        PF_4
-#define APPS2_in        PF_4
-#define APPS_out        PA_5
+#define Steering_WHEEL_PIN      PC_2
+#define BSE_PIN                 PA_0
+#define APPS1_PIN               PF_4
+#define APPS2_PIN               PF_4
+#define APPS_PIN_OUT            PA_5
 
 
-//InterruptIn Velocidade_in(PA_4, PullNone); //(PA_4, PullNone)
-//CAN can1(PD_0,PD_1,500e3);     // Velocidade 1
 
-/*===================================== VCU Sensors Initialization =====================================*/
-BSE_Sensor BSE_1 (BSE_in);
-APP_Sensors APPS (APPS1_in ,APPS2_in, APPS_out);
-Steering_Wheel_Sensor Steering_sensor (Volante_in);
-//Angle_Sensor(PD_0, 1.0, 2.0, 3.0, 4.0);
+//Objects
+//Communication
+motor_can can1(PA_11, PA_12, 1e6);
 
-int main(){
-/*================================== Parameters Initialization ==================================*/
-    float BSE_ang= BSE_1.read();
-    float APPS1_ang = APPS.read_S1();
-    float APPS2_ang = APPS.read_S2();
-    float Steering_ang= Steering_sensor.read();
+//Sensors
+BSE_Sensor BSE (BSE_PIN);
+APP_Sensors APPS (APPS1_PIN ,APPS2_PIN, APPS_PIN_OUT);
+Steering_Wheel_Sensor Steering_sensor (Steering_WHEEL_PIN);
+
+
+int main()
+{
+    can1.set_CAN();
+
+    //Constant Variables
+    const uint16_t Current_inv1{39}, Current_inv2{30};
+    const uint16_t RPM_inv1{9000}, RPM_inv2{9000};
+    uint16_t Wref_1{0},Wref_2{0};
+    float BSE_dg{0} ,Steering_dg{0};
     
-    float Veloc_1{0.1};
-    float Veloc_2{0.1};
-
-    float Volante{0.1};
-    float APPS1{0.1};
-    float APPS2{0.1};
-    float BSE{0.1};
-    
-    //float temperature_c{1.0}; //Temperature in °C
-    //float Linear_Velocity_1{1.0},Linear_Velocity_2{1.0}; //Linear Velocity values in m/s
-    //float Accel_x{0},Accel_y{0}, Accel_z{0}; //Accelaration values in m/s^2
-    //float w_gx{0.0}, w_gy{0}, w_gz{0};        //Angular Velocity values in rad/s
+    //Structs
+    Read_Datafield Inv1_data, Inv2_data;
+    APPS_struct APPS_dg;
+    Wref_struct W_ref;
 
 
 /*================================== LOOP ==================================*/
-    while(true){
-    //Read Sensor data
-        APPS1_ang = APPS.read_S1();
-        APPS2_ang = APPS.read_S2();
-        BSE_ang = BSE_1.read();
-        Steering_ang = Steering_sensor.read();
+    while (true) {
+        //Read all Sensors connected to the VCU
+        APPS_dg= APPS.read_APPS();
+        BSE_dg = BSE.read();
+        Steering_dg = Steering_sensor.read();
+        //LerSensorTemperatura
+        //LerSensoresVelocidade
+        //LerSensoresMPU
 
-        printf("APPS1: %.2f°",APPS1_ang);
-        printf("APPS2: %.2f°",APPS2_ang);
-        printf("BREAK: %.2f°",BSE_ang);  
-        printf("Steering wheel: %.2f°",Steering_ang);     
+        //Calculates Wref via differential
+        W_ref=Differential(Steering_dg,1);
 
+        //Send data to Inverters
+        can1.send_to_inverter(RPM_inv1, W_ref.W1, Current_inv1);
+        can1.send_to_inverter_2(RPM_inv2, W_ref.W2, Current_inv2);
+    
+        //Receive Data from Inverters
+        Inv1_data=can1.receive_from_inverter();
+        Inv2_data=can1.receive_from_inverter_2();
+        
+        //print Received Data
+        can1.Print_Datafields();
 
-/*================================== TO DO ==================================*/
-        //    LerSensoresMPU(mpu9250);
-        //LerSensoresVelocidade();
-        //LerSensorTemperatura();
-        //printf("Velocidade 1 %.2f m/s,Velocidade 2 %.2f m/s \n",Veloc_1, Veloc_2); // Converte a velocidade para RPM
-        //printf("Volante: %.2f°, APPS1: %.2f°, APPS2: %.2f°, BSE: %.2f°\n", Volante,APPS1,APPS2,BSE);
-        //printf("ax = %f, ay = %f, az = %f  m/s²\n", ax * 9.81 - 0.15, ay * 9.81 - 0.1, az * 9.81 + 0.12); //Printa os dados de aceleração convertendo para m/s² e fazendo a conversão
-        //printf("gx = %f, gy = %f, gz = %f  rad/s\n", gx, gy, gz); // Printa os dados de velocidade angular
-        //printf(" temperatura = %f  C\n\r", temperature_c);
-        wait_us(500000);//0.5s
     }
-
-    return 0;
 }
+
