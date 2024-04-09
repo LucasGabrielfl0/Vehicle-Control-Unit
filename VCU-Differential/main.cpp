4d490a7087c7572a676a11fd90c7343754c39fd7
Gabriel <119896525+LucasGabrielfl0@users.noreply.github.com>
2024-04-09 17:43:17 -0300

    Update main.cpp

--- VCU-Differential/main.cpp
+++ VCU-Differential/main.cpp
@@ -3,8 +3,8 @@
  * Federal University of Pernambuco (UFPE)
  * Group Area: Powertrain
  
- * This code should ONLY BE USED FOR TESTING the angler_sensor header file, as it contains many legacy functions no
- * longer used by the team's embedded system's sector(such as 'enviar')
+ * This code should ONLY BE USED FOR TESTING, as it contains many legacy functions no longer used
+ * by the team's embedded system's sector(such as 'enviar')
  ***/
 
 #include "mbed.h"
@@ -15,9 +15,9 @@
 #include <cstdint>
 
 /*===================================== PORTS (STM32 F746ZG)=====================================*/
-#define Volante_in      PC_2
+#define Volante_in      PB_1
 #define BSE_in          PA_0
-#define APPS1_in        PF_4
+#define APPS1_in        PC_2
 #define APPS2_in        PF_4
 #define APPS_out        PA_5
 
@@ -25,6 +25,9 @@
 void enviar(float Volante, float BSE_v, float APPS1_v, float APPS2_v,
             float Veloc_1,float Veloc_2,float ax_v,float ay_v, float az_v,
             float temperature_v, float gx_v, float gy_v, float gz_v);
+
+void LerSensoresMPU(MPU9250& mpu9250_instance);
+
 
 //InterruptIn Velocidade_in(PA_4, PullNone); //(PA_4, PullNone)
 CAN can1(PD_0,PD_1,500e3);     // Velocidade 1
@@ -35,59 +38,101 @@ APP_Sensors APPS (APPS1_in ,APPS2_in, APPS_out);
 Steering_Wheel_Sensor Steering_sensor (Volante_in);
 
 
+//MPU9250 mpu9250;
+
+
 int main(){
 /*================================== Parameters Initialization ==================================*/
+
+/* MPU
+    i2c.frequency(400000); //?
+
+    mpu9250.resetMPU9250();
+    mpu9250.MPU9250SelfTest(SelfTest);
+    mpu9250.initMPU9250();
+    mpu9250.getAres();
+    mpu9250.getGres();
+*/
+
+    can1.mode(CAN::Normal);
+    can1.filter(0,0,CANStandard);
+
+
+   //t.start();
+    APPS_struct Apps_ang= APPS.read_APPS();
     float BSE_ang= BSE_1.read();
-    float APPS1_ang = APPS.read_S1();
-    float APPS2_ang = APPS.read_S2();
     float Steering_ang= Steering_sensor.read();
     
     float Veloc_1{0.1};
     float Veloc_2{0.1};
-
-    float Volante{0.1};
-    float APPS1{0.1};
-    float APPS2{0.1};
-    float BSE{0.1};
     
+
     //float temperature_c{1.0}; //Temperature in °C
     //float Linear_Velocity_1{1.0},Linear_Velocity_2{1.0}; //Linear Velocity values in m/s
     //float Accel_x{0},Accel_y{0}, Accel_z{0}; //Accelaration values in m/s^2
     //float w_gx{0.0}, w_gy{0}, w_gz{0};        //Angular Velocity values in rad/s
 
-
 /*================================== LOOP ==================================*/
     while(true){
-    //Read Sensor data
-        APPS1_ang = APPS.read_S1();
-        APPS2_ang = APPS.read_S2();
+        //Read Sensor data
+        Apps_ang= APPS.read_APPS();
         BSE_ang = BSE_1.read();
         Steering_ang = Steering_sensor.read();
 
-        printf("APPS1: %.2f°",APPS1_ang);
-        printf("APPS2: %.2f°",APPS2_ang);
-        printf("BREAK: %.2f°",BSE_ang);  
-        printf("Steering wheel: %.2f°",Steering_ang);     
+
+        //Read_MPU
 
 
-/*================================== TO DO ==================================*/
-        //    LerSensoresMPU(mpu9250);
+/*================================== PRINT ==================================*/
+        //LerSensoresMPU(mpu9250);
         //LerSensoresVelocidade();
         //LerSensorTemperatura();
         //printf("Velocidade 1 %.2f m/s,Velocidade 2 %.2f m/s \n",Veloc_1, Veloc_2); // Converte a velocidade para RPM
-        //printf("Volante: %.2f°, APPS1: %.2f°, APPS2: %.2f°, BSE: %.2f°\n", Volante,APPS1,APPS2,BSE);
-        //printf("ax = %f, ay = %f, az = %f  m/s²\n", ax * 9.81 - 0.15, ay * 9.81 - 0.1, az * 9.81 + 0.12); //Printa os dados de aceleração convertendo para m/s² e fazendo a conversão
+        printf("\n===========================================================================\n");        
+        printf("[VCU] Volante: %.2f , APPS1: %.2f , APPS2: %.2f , BSE: %.2f \n", Steering_ang, Apps_ang.s1, Apps_ang.s2, BSE_ang);
+        Steering_sensor.Voltage_print();
+        printf("\n===========================================================================\n");
+        
+        //printf("ax = %f, ay = %f, az = %f  m/s²\n", ax * 9.81 - 0.15, ay * 9.81 - 0.1, az * 9.81 + 0.12);
         //printf("gx = %f, gy = %f, gz = %f  rad/s\n", gx, gy, gz); // Printa os dados de velocidade angular
         //printf(" temperatura = %f  C\n\r", temperature_c);
-        wait_us(500000);//0.5s
+        wait_us(1000000);//0.5s
     }
 
     return 0;
 }
 
 
+/*================================== aux (legacy) Functions ==================================*/
+// Função para leitura dos sensores MPU9250
+/*
+void LerSensoresMPU(MPU9250& mpu9250_instance) {
 
-//aux (legacy) Functions
+      // Se o pino for para HIGh, todo os registradores recebem novos dados
+        if(mpu9250.readByte(MPU9250_ADDRESS, INT_STATUS) & 0x01) {  // Na interrupção, checa se a data está pronta para interromper
+            
+            mpu9250.readAccelData(accelCount);  // Lê os valores x/y/z do adc   
+            // Calcula a aceleração em g's
+           ax = (float)accelCount[0] * aRes - accelBias[0];
+            ay = (float)accelCount[1] * aRes - accelBias[1];   
+            az = (float)accelCount[2] * aRes - accelBias[2];  
+    
+            mpu9250.readGyroData(gyroCount);  // Lê os valores x/y/z do adc  
+            // Calcula a velocidade angular em graus por segundo
+            gx = (float)gyroCount[0] * gRes - gyroBias[0];
+            gy = (float)gyroCount[1] * gRes - gyroBias[1];  
+            gz = (float)gyroCount[2] * gRes - gyroBias[2];   
+        }
+
+        // Normalização e conversão dos valores obtidos
+        mpu9250.MahonyQuaternionUpdate(ax, ay, az, gx*PI/180.0f, gy*PI/180.0f, gz*PI/180.0f, 0, 0, 0);
+
+}
+
+*/
+
+
+
 void enviar(float Volante, float BSE_v, float APPS1_v, float APPS2_v,
             float Veloc_1,float Veloc_2,float ax_v,float ay_v, float az_v,
             float temperature_v, float gx_v, float gy_v, float gz_v){
