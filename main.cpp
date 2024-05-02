a6342a18c500569bc6ce785c51bae895d2bd2fcf
Gabriel <119896525+LucasGabrielfl0@users.noreply.github.com>
2024-05-02 20:00:14 -0300

    add mpu9250 methods

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@
  
  * This file contains the Integrated VCU (VEHICLE CONTROL UNIT) that unites the
  * CAN communication with Inverters, the analog communication with all the sensors and 
- * the Electronic Differential Closed Loop control
+ * the Electronic Differential Closed Loop control algorithm
  ***/
 
 #include "mbed.h"
@@ -26,10 +26,13 @@
 //Communication
 motor_can can1(PA_11, PA_12, 1e6);
 
-//Sensors
+//Angle Sensors
 BSE_Sensor BSE (BSE_PIN);
 APP_Sensors APPS (APPS1_PIN ,APPS2_PIN, APPS_PIN_OUT);
 Steering_Wheel_Sensor Steering_sensor (Steering_WHEEL_PIN);
+
+//Gyro Sensor
+//mpu
 
 
 int main()
@@ -43,23 +46,32 @@ int main()
     float BSE_dg{0} ,Steering_dg{0};
     
     //Structs
-    RxDatafield_struct Inv1_data, Inv2_data;
+    Rx_struct Inv1_data, Inv2_data;
     APPS_struct APPS_dg;
     Wref_struct W_ref;
 
     /*================================== LOOP ==================================*/
     while (true) {
-        //Read all Sensors connected to the VCU
+        //Read all Angle Sensors connected to the VCU
         APPS_dg= APPS.read_APPS();
         BSE_dg = BSE.read();
         Steering_dg = Steering_sensor.read();
-        //LerSensorTemperatura
-        //LerSensoresVelocidade
-        //LerSensoresMPU
+        
+        //Read MPU9250
+        //MPU9250: Accelerometer
+
+
+        //MPU9250: Angular Velocity         
+
+
+        //MPU9250: Temperature       
+
+
 
         //Calculates Wref via differential
-        W_ref=Differential(Steering_dg,1);
-
+//        W_ref=Differential(Steering_dg,1);
+//        printf("W1= %.d rad/s W2= %.d rad/s",W_ref.W1,W_ref.W2 );
+        
         //Send data to Inverters
         can1.send_to_inverter(RPM_inv1, W_ref.W1, Current_inv1);
         can1.send_to_inverter_2(RPM_inv2, W_ref.W2, Current_inv2);
@@ -67,7 +79,13 @@ int main()
         //Receive Data from Inverters
         Inv1_data=can1.receive_from_inverter();
         Inv2_data=can1.receive_from_inverter_2();
-        
+
+        //Datalogger
+
+
+        //Telemetry
+
+
         //print Received Data
         can1.Print_Datafields();
 
