3b15866ca01ad20a2e580c18b1dc37999c0d52c4
LucasGabrielfl0 <lucasgabrielfl00@gmail.com>
2024-05-07 02:04:46 -0300

    Creating new Structs

--- motor_can.h
+++ motor_can.h
@@ -32,8 +32,8 @@
 #define SENSOR_MAX_OFFSET_5V 65535
 
 /*================================== Receive Struct ==================================*/
-struct Read_Datafield{
-    //Data receive from the motor controller (In order)
+struct Rx_struct{
+    //Receiver Datafield in CAN 2.0 standard (In order)
     uint8_t     Msg_Counter{0};        //[0]
     float       Input_Voltage{0};      //[1]
     uint8_t     Temp_Controller{0};
@@ -44,8 +44,8 @@ struct Read_Datafield{
 };
 
 /*================================== Send Struct ==================================*/
-struct Write_Datafield{
-    //Data receive from the motor controller (In order)
+struct Tx_struct{
+    //Transceiver Datafield in CAN 2.0 standard (In order)
     uint16_t    RPM_Limit{0};
     //motor pole pair is a constant
     uint16_t    Tx_PWM{0};
@@ -59,9 +59,9 @@ struct Write_Datafield{
 /*================================== CLASS ==================================*/
 class motor_can:public CAN{
 
-   // private:
-    Read_Datafield Datafield_inv1;
-    Read_Datafield Datafield_inv2;
+    private:
+    Rx_struct Datafield_inv1; //saves the datafield received from Inverter 1
+    Rx_struct Datafield_inv2; //saves the datafield received from Inverter 2
 
     //Methods
     public:
@@ -69,15 +69,19 @@ class motor_can:public CAN{
     void reset_can();
     bool baud_test();
     bool is_can_active();
+    
     //Send data to both motor Controllers
-    void send_to_inverter(uint16_t rpm_1, uint16_t Angle_1, uint16_t current_1 );
-    void send_to_inverter_2(uint16_t rpm_2, uint16_t Angle_2, uint16_t current_2 );
+    void send_to_inverter(uint16_t rpm_1, uint16_t pwm_1, uint16_t current_1 );
+    void send_to_inverter_2(uint16_t rpm_2, uint16_t pwm_2, uint16_t current_2 );
+
     // Receive data from both motor controllers
-    Read_Datafield receive_from_inverter();
-    Read_Datafield receive_from_inverter_2();
+    Rx_struct receive_from_inverter();
+    Rx_struct receive_from_inverter_2();
+    
     //print received data
     void Print_Datafields();
-    void Print_Datafield(int Num, Read_Datafield Inv);
+    void Print_Datafield(int Num, Rx_struct Inv);
+    
     //Constructors
     public:
     motor_can(PinName _can_pin_rx, PinName _can_pin_tx, uint32_t _can_frequency);
@@ -87,6 +91,7 @@ class motor_can:public CAN{
 /*================================== Constructors ==================================*/
 inline motor_can::motor_can(PinName _can_pin_rx, PinName _can_pin_tx, uint32_t _can_frequency)
 :CAN(_can_pin_rx,_can_pin_tx, _can_frequency) {}
+
 /*================================== METHODS ==================================*/
 inline void motor_can:: set_CAN(){
     mode(CAN::Normal);
@@ -116,7 +121,7 @@ inline bool motor_can::is_can_active(){
 }
 
 
-inline void motor_can:: send_to_inverter(uint16_t rpm_1, uint16_t Angle_1, uint16_t current_1 ){
+inline void motor_can:: send_to_inverter(uint16_t rpm_1, uint16_t pwm_1, uint16_t current_1 ){
     CANMessage inverter_tx_msg;
     inverter_tx_msg.id = INVERSOR_TX_ID;
     inverter_tx_msg.len = 8; // Define o tamanho da msg, max = 8 Bytes
@@ -124,15 +129,15 @@ inline void motor_can:: send_to_inverter(uint16_t rpm_1, uint16_t Angle_1, uint1
     inverter_tx_msg.data[0] = rpm_1 & 0xFF; //  Byte menos significativos do RPM
     inverter_tx_msg.data[1] = rpm_1 >> 8; // Byte mais significativos do RPM
     inverter_tx_msg.data[2] = N_Poles_Motor; // Numero de pares de polos do motor
-    inverter_tx_msg.data[3] = Angle_1 & 0xFF; // Byte menos significativo do PWM
-    inverter_tx_msg.data[4] = Angle_1 >> 8; // Byte mais significativo do PWM
+    inverter_tx_msg.data[3] = pwm_1 & 0xFF; // Byte menos significativo do PWM
+    inverter_tx_msg.data[4] = pwm_1 >> 8; // Byte mais significativo do PWM
     inverter_tx_msg.data[5] = current_1 & 0xFF; // Byte menos significativo corrente;
     inverter_tx_msg.data[6] = current_1 >> 8; // Byte mais significativo da corrente;
     inverter_tx_msg.data[7] = 0b00000000; 
 
 }
 
-inline void motor_can:: send_to_inverter_2(uint16_t rpm_2, uint16_t Angle_2, uint16_t current_2 ){
+inline void motor_can:: send_to_inverter_2(uint16_t rpm_2, uint16_t pwm_2, uint16_t current_2 ){
     CANMessage inverter_tx_msg_2;
     inverter_tx_msg_2.id = INVERSOR_TX_ID_2;
     inverter_tx_msg_2.len = 8; // Define o tamanho da msg, max = 8 Bytes
@@ -140,8 +145,8 @@ inline void motor_can:: send_to_inverter_2(uint16_t rpm_2, uint16_t Angle_2, uin
     inverter_tx_msg_2.data[0] = rpm_2 & 0xFF; //  Byte menos significativos do RPM
     inverter_tx_msg_2.data[1] = rpm_2 >> 8; // Byte mais significativos do RPM
     inverter_tx_msg_2.data[2] = N_Poles_Motor; // Numero de pares de polos do motor
-    inverter_tx_msg_2.data[3] = Angle_2 & 0xFF; // Byte menos significativo do PWM
-    inverter_tx_msg_2.data[4] = Angle_2 >> 8; // Byte mais significativo do PWM
+    inverter_tx_msg_2.data[3] = pwm_2 & 0xFF; // Byte menos significativo do PWM
+    inverter_tx_msg_2.data[4] = pwm_2 >> 8; // Byte mais significativo do PWM
     inverter_tx_msg_2.data[5] = current_2 & 0xFF; // Byte menos significativo corrente;
     inverter_tx_msg_2.data[6] = current_2 >> 8; // Byte mais significativo da corrente;
     inverter_tx_msg_2.data[7] = 0b00000000; 
@@ -149,9 +154,9 @@ inline void motor_can:: send_to_inverter_2(uint16_t rpm_2, uint16_t Angle_2, uin
 }
 
 
-inline Read_Datafield motor_can:: receive_from_inverter() {
+inline Rx_struct motor_can:: receive_from_inverter() {
     //Definir mensagem CAN a ser recebida
-    Read_Datafield Datafield;
+    Rx_struct Datafield;
 
     //Aux variables
     int Voltage_Hb; //voltage High byte
@@ -188,7 +193,48 @@ inline Read_Datafield motor_can:: receive_from_inverter() {
     Datafield_inv1 =Datafield;
     return Datafield;
 
-   }
+}
+
+inline Rx_struct motor_can:: receive_from_inverter_2() {
+    //Definir mensagem CAN a ser recebida
+    Rx_struct Datafield;
+
+    //Aux variables
+    int Voltage_Hb; //voltage High byte
+    int Voltage_int;
+
+    CANMessage inverter_rx_msg2;
+    inverter_rx_msg2.len = 8;
+    if(is_can_active()) {
+        if(read(inverter_rx_msg2)){
+        // Aguardar a recepção da mensagem do inversor
+            if(inverter_rx_msg2.id == INVERSOR_RX_ID_2) {
+                Datafield.Msg_Counter = inverter_rx_msg2.data[0] & 0xF;
+                
+                Voltage_Hb = inverter_rx_msg2.data[0] >> 4;
+                Voltage_int = (Voltage_Hb<<8) | inverter_rx_msg2.data[1];
+                Datafield.Input_Voltage = Voltage_int/10.0f;
+
+                Datafield.Temp_Controller = inverter_rx_msg2.data[2]-100; //Range[0-255],Temp Range [-100°C to 155°C]
+                Datafield.Temp_motor = inverter_rx_msg2.data[3]-100; //Range[0-255],Temp Range [-100°C to 155°C]
+                
+                Datafield.RPM= (inverter_rx_msg2.data[5]<< 8) | inverter_rx_msg2.data[4] ;
+                
+                Datafield.rx_PWM=(inverter_rx_msg2.data[6]/255.0f)*100;
+                Datafield.Current = inverter_rx_msg2.data[7];            
+            
+            }
+        }
+    }
+    else{
+        printf("Fail to stablish CAN connection. Reseting...\n");
+        reset_can(); 
+    }
+    
+    Datafield_inv2 =Datafield;
+    return Datafield;
+
+}
 
 
 inline void motor_can::Print_Datafields(){
@@ -197,7 +243,7 @@ inline void motor_can::Print_Datafields(){
     
 }
 
-inline void Print_Datafield(int Num, Read_Datafield Inv){
+inline void Print_Datafield(int Num, Rx_struct Inv){
     printf("\r\n\t[CAN] Inverter %d: Volt=%.1f V, T_Ctrl= %d°C ,T_Motor = %d°C , RPM = %d, PWM = %d (%.2f %%), Ic= %d A",
     Num, Inv.Input_Voltage ,Inv.Temp_Controller ,
          Inv.Temp_motor    ,Inv.RPM ,
